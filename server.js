09942024c568dd9e1e02fbfe8f5a3deb8f0928e8
Marcelo Mesquita <mm.marcelomesquita@gmail.com>
2023-07-06 14:12:12 -0300

    Boilerplate ready

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const routes = require('./routes')
 const path = require('path')
 
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.resolve(__dirname, 'public')))
 app.use(routes)
 
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
-
-
 
 const porta = 3000
 
