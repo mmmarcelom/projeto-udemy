607d89ddb96a25b44d15b8273c425c8f3cbd85b6
Marcelo Mesquita <mm.marcelomesquita@gmail.com>
2023-07-06 17:55:35 -0300

    Mongo conectado

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
 const routes = require('./routes')
 const path = require('path')
+
+const mongoose = require('mongoose')
+const mongoOptions = {useNewUrlParser : true, useUnifiedTopology: true}
+
+mongoose
+    .connect(process.env.CONNECTIONSTRING, mongoOptions)
+    .then(() => {
+        console.log('Iniciando a conexão com MongoDB...')
+        app.emit('Ready')
+    }).catch(e => console.log('Erro', e))
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.resolve(__dirname, 'public')))
@@ -12,7 +25,10 @@ app.set('view engine', 'ejs')
 
 const porta = 3000
 
-app.listen(porta, ()=>{
-    console.log(`Iniciando servidor na porta ${porta}`)
-    console.log(`http://localhost:${porta}`)  
+app.on('Ready', () => {
+    console.log('Conexão realizada com sucesso\n')
+    app.listen(porta, ()=>{
+        console.log(`Iniciando servidor na porta ${porta}`)
+        console.log(`http://localhost:${porta}`)
+    })
 })
