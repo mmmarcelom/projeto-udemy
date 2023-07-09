7cb636cd5bae9d58b8ff02fc47f9a698beac87ae
Marcelo Mesquita <mm.marcelomesquita@gmail.com>
2023-07-09 13:15:42 -0300

    Adicionado estilos JS

--- server.js
+++ server.js
@@ -2,39 +2,33 @@ const path = require('path')
 require('dotenv').config()
 
 const express = require('express')
-const session = require('express-session');
+const session = require('express-session')
 
 const app = express()
 
-const mongoStore = require('connect-mongo');
+const mongoStore = require('connect-mongo')
 const mongoose = require('mongoose')
 mongoose
-    .connect(
-        process.env.CONNECTIONSTRING, 
-        { useNewUrlParser : true, useUnifiedTopology: true })
-    .then(()=> console.log('Conectado ao Mongo DB') )
-    .catch(e => console.log('Falha ao conectar no Mongo DB'))
+  .connect(
+    process.env.CONNECTIONSTRING,
+    { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conectado ao Mongo DB'))
+  .catch(e => console.log('Falha ao conectar no Mongo DB'))
 
 app.use(session({
-    secret: '123456',
-    store: mongoStore.create(mongoose.connection),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        httpOnly: true,
-        maxAge: (
-            7 /* dias */ *
-            24 /* horas */ *
-            60 /* minutos */ *
-            60 /* segundos */ *
-            1000 /* milisegundos */ 
-            )
-        }
-    }))
-        
+  secret: '123456',
+  store: mongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  }
+}))
+
 app.use(require('connect-flash')())
 app.use(require('./routes'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.resolve(__dirname, 'public')))
 
 app.set('views', path.resolve(__dirname, 'src', 'views'))
@@ -42,7 +36,7 @@ app.set('view engine', 'ejs')
 
 const porta = 3000
 
-app.listen(porta, ()=>{
-    console.log(`Iniciando servidor na porta ${porta}`)
-    console.log(`http://localhost:${porta}`)
+app.listen(porta, () => {
+  console.log(`Iniciando servidor na porta ${porta}`)
+  console.log(`http://localhost:${porta}`)
 })
